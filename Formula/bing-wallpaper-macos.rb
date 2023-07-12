4132f5d3e897b140445b11a78c01472e4cf05cb3
moesnow <11678347+moesnow@users.noreply.github.com>
2023-07-12 10:45:12 +0800

    release bing-wallpaper-macos v0.0.2

--- Formula/bing-wallpaper-macos.rb
+++ Formula/bing-wallpaper-macos.rb
@@ -1,9 +1,9 @@
 class BingWallpaperMacos < Formula
-  version "0.0.1"
+  version "0.0.2"
   desc "Automatically set bing wallpaper every day on macOS"
   homepage "https://github.com/moesnow/bing-wallpaper-macos"
-  url "https://github.com/moesnow/bing-wallpaper-macos/archive/refs/tags/v0.0.1.tar.gz"
-  sha256 "5531ec08d026108d7efbe4011651e409bdbed3f7948175fc7b338ef105f67029"
+  url "https://github.com/moesnow/bing-wallpaper-macos/archive/refs/tags/v0.0.2.tar.gz"
+  sha256 "c1bb0d07618cecb4dd0257d3d50eade708282082a54fae9f1cf4b0d22fdab26a"
 
   depends_on "nlohmann-json" => :build
 
@@ -13,9 +13,13 @@ class BingWallpaperMacos < Formula
   end
 
   service do
-    run [opt_bin/"bing-wallpaper-macos"]
+    run [opt_bin/"bing-wallpaper-macos", "--auto"]
     run_type :interval
     interval 300
     require_root false
+  end
+
+  test do
+    assert_match "Program" , shell_output("#{bin}/bing-wallpaper-macos --version")
   end
 end
