e417f2f26e23cf456ba93f1caabc9f92d0aaca3e
moesnow <11678347+moesnow@users.noreply.github.com>
2023-07-26 14:11:04 +0800

    release hitokoto v0.0.2

--- Formula/hitokoto.rb
+++ Formula/hitokoto.rb
@@ -1,9 +1,9 @@
 class Hitokoto < Formula
-  version "0.0.1"
+  version "0.0.2"
   desc "Get a hitokoto in the terminal"
   homepage "https://github.com/moesnow/hitokoto"
-  url "https://github.com/moesnow/hitokoto/archive/refs/tags/v0.0.1.tar.gz"
-  sha256 "2681707ad5b7fe3276e61fb6ab878af72d82ca42b0e3efa793afc9545afa1937"
+  url "https://github.com/moesnow/hitokoto/archive/refs/tags/v0.0.2.tar.gz"
+  sha256 "371facd108c57aa3713bd3221483b1302be24d4c53c3aa326b6b40e1d96db0bd"
 
   depends_on "json-c" => :build
 
